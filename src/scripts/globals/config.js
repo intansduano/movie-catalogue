9485cf1eb0f687f7165244cf244d1644e6735d78
intan d <intan.duano27@gmail.com>
2023-04-21 10:01:16 +0800

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    KEY: 'f694786bf1f40c5bf3a936e1dfb1651d',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+    CACHE_NAME: new Date().toISOString(),
+};
+
+export default CONFIG;
---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,23 @@
+import UrlParser from '../../routes/url-parser';
+import TheMovieDbSource from '../../data/themoviedb-source';
+import { createMovieDetailTemplate } from '../templates/template-creator';
+
+const Detail = {
+    async render() {
+        return `
+        <div id="movie" class="movie"></div>
+    `;
+    },
+
+    async afterRender() {
+        // Fungsi ini akan dipanggil setelah render()
+        const url = UrlParser.parseActiveUrlWithoutCombiner();
+        const movie = await TheMovieDbSource.detailMovie(url.id);
+
+        // TODO: tampilkan movie di dalam DOM
+        const movieContainer = document.querySelector('#movie');
+        movieContainer.innerHTML = createMovieDetailTemplate(movie);
+    },
+};
+
+export default Detail;
